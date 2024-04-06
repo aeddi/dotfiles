bc7d7ee5387fb92d1c0fe9b2b8babd39f00ab765
aeddi <antoine.e.b@gmail.com>
2024-04-06 11:14:57 +0200

    feat(iterm): update config

---
+++ raycast/govee-off.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Govee Off
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Turn off Govee TV Led Strip
+# @raycast.author aeddi
+
+zsh -lic 'govee_off'
---
+++ raycast/govee-on.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Govee On
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Turn on Govee TV Led Strip
+# @raycast.author aeddi
+
+zsh -lic 'govee_on'
