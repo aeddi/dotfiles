fd8dba49279be8c8154658f4985ae700ba32454a
aeddi <antoine.e.b@gmail.com>
2024-04-11 15:05:32 +0200

    feat(vim): customize telescope config

---
+++ vim/config/lua/plugins/telescope.lua
@@ -0,0 +1,52 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "debugloop/telescope-undo.nvim",
+  },
+  config = function()
+    local actions = require("telescope.actions")
+    require("telescope").setup({
+      defaults = {
+        mappings = {
+          n = {
+            -- Close Telescope using ;j in normal mode
+            [";j"] = actions.close,
+
+            ["<C-u>"] = actions.preview_scrolling_up,
+            ["<C-d>"] = actions.preview_scrolling_down,
+            ["<C-f>"] = actions.preview_scrolling_left,
+            ["<C-b>"] = actions.preview_scrolling_right,
+          },
+          i = {
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-l>"] = actions.select_default,
+
+            ["<C-S-h>"] = actions.results_scrolling_left,
+            ["<C-S-l>"] = actions.results_scrolling_right,
+
+            ["<C-u>"] = actions.preview_scrolling_up,
+            ["<C-d>"] = actions.preview_scrolling_down,
+            ["<C-f>"] = actions.preview_scrolling_left,
+            ["<C-b>"] = actions.preview_scrolling_right,
+          },
+        },
+        layout_config = {
+          preview_width = 0.6,
+        },
+      },
+      -- Setup Telescope undo
+      extensions = {
+        undo = {
+          use_delta = false,
+        },
+      },
+    })
+    require("telescope").load_extension("undo")
+  end,
+  keys = {
+    { "<leader>U", "<cmd>Telescope undo<cr>", desc = "Telescope Undo" },
+    { "U", "<cmd>Telescope undo<cr>", desc = "Telescope Undo" },
+  },
+}
